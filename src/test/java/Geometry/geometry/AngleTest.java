5a31b6ab660159605ead9227f8e985004af68b99
Rinzler <varunb.london2019a@gmail.com>
2020-01-07 15:24:35 +0000

    JRE doesnt work

---
+++ src/test/java/Geometry/geometry/AngleTest.java
@@ -0,0 +1,23 @@
+package Geometry.geometry;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AngleTest 
+{
+    @BeforeEach
+	    public void setUp() throws Exception 
+    {
+	    }
+    @Test
+	    public void test() 
+	    {
+	        fail("Not yet implemented");
+	    }
+}
+
