bdfb66f1f486b8a2dee97cce41063324e241408f
Rinzler <varunb.london2019a@gmail.com>
2020-01-07 15:03:14 +0000

    pasted in code

---
+++ src/main/java/Geometry/geometry/Angle.java
@@ -0,0 +1,18 @@
+package Geometry.geometry;
+
+public class Angle 
+{
+    private int degrees;     
+    public int getDegrees()
+    {
+        return degrees;
+    }
+    private void setDegrees(int degrees)
+    {
+        this.degrees = degrees;
+    }
+    public Angle(int degrees)
+    {
+    	  setDegrees(degrees) ;
+    }
+}
---
+++ src/main/java/Geometry/geometry/App.java
@@ -0,0 +1,13 @@
+package Geometry.geometry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
